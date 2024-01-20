22ba237ed5ec5dfc584cfa8e86c74362b5406f05
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2024-01-20 23:30:45 +0200

    Added server folder and fetch it

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import products from "./data/Products.js";
+
+const app = express()
+
+app.get("/api/products",(req,res)=>{
+    res.json(products)
+});
+
+app.get("/api/product/:id",(req,res)=>{
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
+
+
+app.get("/",(req,res)=>{
+    res.send("API is Running...")
+});
+
+app.listen(5000,console.log("server running port 5000..."))
